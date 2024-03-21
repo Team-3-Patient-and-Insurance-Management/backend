4c772a2271632da34491d09e98358e39a357ecbd
Sasha <svelet@iu.edu>
2024-03-21 16:23:53 -0400

    added crossOrigin so that front end can access

--- src/main/java/com/java/firebase/controllers/Patient/BookingController.java
+++ src/main/java/com/java/firebase/controllers/Patient/BookingController.java
@@ -2,13 +2,11 @@ package com.java.firebase.controllers.Patient;
 
 import com.java.firebase.service.Patient.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ExecutionException;
 @RestController
+@CrossOrigin
 public class BookingController {
 
     private final BookingService bookingService;
--- src/main/java/com/java/firebase/controllers/Patient/DoctorSearchController.java
+++ src/main/java/com/java/firebase/controllers/Patient/DoctorSearchController.java
@@ -5,6 +5,7 @@ import com.java.firebase.service.Patient.DoctorSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @RestController
+@CrossOrigin
 public class DoctorSearchController {
 
     @Autowired
