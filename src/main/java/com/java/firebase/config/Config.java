1e3201eb48f43584ce93eec4e13cdf83e2950708
Marvell <vmarvellinus@gmail.com>
2024-04-05 15:13:49 -0400

    added cors (?)

---
+++ src/main/java/com/java/firebase/config/Config.java
@@ -0,0 +1,22 @@
+package com.java.firebase.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class Config implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "patientinsurancemanagement.web.app",
+                        "patientinsurancemanagement.firebaseapp.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
