61c51dca78d1effdbd4d7ead4eb5c1c9da3a9957
Marvell <vmarvellinus@gmail.com>
2024-04-18 15:06:28 -0400

    patient insurance provider debug

--- src/main/java/com/java/firebase/model/Patient/PatientInsuranceProviders.java
+++ src/main/java/com/java/firebase/model/Patient/PatientInsuranceProviders.java
@@ -5,52 +5,101 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PatientInsuranceProviders {
-    private String insuranceProvider;
-    private String policyName;
-    private String representative;
+    private Double deductible;
+    private Boolean dentalCoverage;
+    private String insurancePlanId;
     private String description;
-    private String price;
+    private String insuranceProviderUid;
+    private Boolean medicalCoverage;
+    private String planName;
+    private Double premium;
+    private Boolean visionCoverage;
 
-    public PatientInsuranceProviders(String insuranceProvider, String policyName, String representative,
-                                     String description, String price) {
-        this.insuranceProvider = insuranceProvider;
-        this.policyName = policyName;
-        this.representative = representative;
+    public Double getDeductible() {
+        return deductible;
+    }
+
+    public void setDeductible(Double deductible) {
+        this.deductible = deductible;
+    }
+
+    public Boolean getDentalCoverage() {
+        return dentalCoverage;
+    }
+
+    public void setDentalCoverage(Boolean dentalCoverage) {
+        this.dentalCoverage = dentalCoverage;
+    }
+
+    public String getInsurancePlanId() {
+        return insurancePlanId;
+    }
+
+    public void setInsurancePlanId(String insurancePlanId) {
+        this.insurancePlanId = insurancePlanId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
-        this.price = price;
+    }
+
+    public String getInsuranceProviderUid() {
+        return insuranceProviderUid;
+    }
+
+    public void setInsuranceProviderUid(String insuranceProviderUid) {
+        this.insuranceProviderUid = insuranceProviderUid;
+    }
+
+    public Boolean getMedicalCoverage() {
+        return medicalCoverage;
+    }
+
+    public void setMedicalCoverage(Boolean medicalCoverage) {
+        this.medicalCoverage = medicalCoverage;
+    }
+
+    public String getPlanName() {
+        return planName;
+    }
+
+    public void setPlanName(String planName) {
+        this.planName = planName;
+    }
+
+    public Double getPremium() {
+        return premium;
+    }
+
+    public void setPremium(Double premium) {
+        this.premium = premium;
+    }
+
+    public Boolean getVisionCoverage() {
+        return visionCoverage;
+    }
+
+    public void setVisionCoverage(Boolean visionCoverage) {
+        this.visionCoverage = visionCoverage;
+    }
+
+    public PatientInsuranceProviders(Double deductible, Boolean dentalCoverage, String insurancePlanId, String description, String insuranceProviderUid, Boolean medicalCoverage, String planName, Double premium, Boolean visionCoverage) {
+        this.deductible = deductible;
+        this.dentalCoverage = dentalCoverage;
+        this.insurancePlanId = insurancePlanId;
+        this.description = description;
+        this.insuranceProviderUid = insuranceProviderUid;
+        this.medicalCoverage = medicalCoverage;
+        this.planName = planName;
+        this.premium = premium;
+        this.visionCoverage = visionCoverage;
     }
 
     public PatientInsuranceProviders() {
     }
 
-    public String getInsuranceProvider() {
-        return insuranceProvider;
-    }
-    public void setInsuranceProvider(String insuranceProvider) {
-        this.insuranceProvider = insuranceProvider;
-    }
-    public String getPolicyName() {
-        return policyName;
-    }
-    public void setPolicyName(String policyName) {
-        this.policyName = policyName;
-    }
-    public String getRepresentative() {
-        return representative;
-    }
-    public void setRepresentative(String representative) {
-        this.representative = representative;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public String getPrice() {
-        return price;
-    }
-    public void setPrice(String price) {
-        this.price = price;
-    }
 }
